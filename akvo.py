66e0a838892431f07408de3d6bd7f22f66133332
Wojciech Siemiątkowski <s20404@pjwstk.edu.pl>
2024-11-14 21:32:48 +0100

    Fixes #1

--- akvo.py
+++ akvo.py
@@ -4,8 +4,9 @@ import streamlit as st
 # 1 ppm Ca = 2.5 ppm CaCO3
 # 1 ppm Mg = 4.1 ppm CaCO3
 
-CA_CACO3_CONVERSION = 2.5
-MG_CACO3_CONVERSION = 4.1
+CA_CACO3_CONVERSION = 2.497
+MG_CACO3_CONVERSION = 4.118
+HC03_CONVERSION = 0.8202
 
 # Concentration in buffers (in mg of element per gram of buffer)
 
@@ -17,13 +18,13 @@ MAGNESIUM_BUFFER_CONCENTRATION = 1
 CALCIUM_BUFFER_CONCENTRATION = 1
 
 def calculate_buffer_requirements(target_gh, target_kh, water_volume, ca_ratio, mg_ratio, k_ratio):
-    target_ca_caco3 = target_gh * (ca_ratio / (ca_ratio + mg_ratio))
-    target_mg_caco3 = target_gh * (mg_ratio / (ca_ratio + mg_ratio))
+    target_ca_caco3 = target_gh * (ca_ratio / 100)
+    target_mg_caco3 = target_gh * (mg_ratio / 100)
     target_k_caco3 = target_kh * k_ratio
 
     target_ca_ppm = target_ca_caco3 / CA_CACO3_CONVERSION
     target_mg_ppm = target_mg_caco3 / MG_CACO3_CONVERSION
-    target_k_ppm = target_k_caco3  # Potassium is directly in ppm
+    target_k_ppm = target_k_caco3  / HC03_CONVERSION
 
     required_ca_mg = target_ca_ppm * (water_volume / 1000)
     required_mg_mg = target_mg_ppm * (water_volume / 1000)
@@ -46,13 +47,6 @@ st.write("Specify the desired proportions for GH components (Calcium and Magnesi
 ca_ratio = st.number_input("Calcium proportion (as % of GH)", min_value=0.0, max_value=100.0, value=66.7)
 mg_ratio = st.number_input("Magnesium proportion (as % of GH)", min_value=0.0, max_value=100.0, value=33.3)
 k_ratio = st.number_input("Potassium proportion (as % of KH)", min_value=0.0, max_value=100.0, value=100.0)
-
-if ca_ratio + mg_ratio > 0:
-    ca_ratio = ca_ratio / (ca_ratio + mg_ratio)
-    mg_ratio = mg_ratio / (ca_ratio + mg_ratio)
-
-if k_ratio > 0:
-    k_ratio = k_ratio / 100.0  # Since potassium is only for KH
 
 ca_buffer, mg_buffer, k_buffer = calculate_buffer_requirements(target_gh, target_kh, water_volume, ca_ratio, mg_ratio, k_ratio)
 
